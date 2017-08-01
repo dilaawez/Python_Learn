95942f6303502fe4521c1b5cc4ce1df6fd9f1af4
vuln3rability <Dilaawez.Khan@Outlook.com>
2017-08-01 23:50:37 +0530

    Add files via upload

---
+++ Temperature.py
@@ -0,0 +1,6 @@
+print ("This program converts Fahrenheit to celsius")
+print ("Type in a temperature in Fahrenheit:",end=" ")
+fahrenheit = float (input())
+celsius = (fahrenheit - 32) * 5.0 / 9
+print ("that is", celsius,"degree Celsius")
+
